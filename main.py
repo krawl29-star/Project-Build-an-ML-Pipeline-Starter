70bb3cb8e1e385846abf6e64500847f0ffbd4836
Katelynn Rawlings <krawl29@wgu.edu>
2025-08-14 17:20:50 -0400

    Step 4 commit Data Split and train RandomForest

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            mlflow.run(
+            _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
             entry_point="main",
             parameters={
@@ -65,7 +65,7 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            mlflow.run(
+            _ = mlflow.run(
                      os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
                      "main",
                      parameters={
@@ -78,10 +78,16 @@ def go(config: DictConfig):
                  )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+            os.path.join(config['main']['components_repository'], "train_val_test_split"),
+            'main',
+            parameters = {
+                "input":"clean_sample.csv:latest",
+                "test_size": config['modeling']['test_size'],
+                "random_seed": config['modeling']['random_seed'],
+                "stratify_by": config['modeling']['stratify_by']
+            }
+)
 
         if "train_random_forest" in active_steps:
 
@@ -96,8 +102,19 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                'main',
+                parameters={
+                "trainval_artifact":"trainval_data.csv:latest",
+                "val_size": config['modeling']['val_size'],
+                "random_seed": config['modeling']['random_seed'],
+                "stratify_by": config['modeling']['stratify_by'],
+                "rf_config": rf_config,
+                "max_tfidf_features": config['modeling']['max_tfidf_features'],
+                "output_artifact":"random_forest_export.csv"
+                },
+             )
 
         if "test_regression_model" in active_steps:
 
